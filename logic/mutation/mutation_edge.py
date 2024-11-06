f11a56c71b70e1779fe988bcf637edf10c6e9a05
mkrolczykk <m.krolczyk66@gmail.com>
2024-11-06 19:06:26 +0100

    added edge mutation

---
+++ logic/mutation/mutation_edge.py
@@ -0,0 +1,16 @@
+import random
+from logic.mutation.mutation_algorithm import MutationAlgorithm
+from logic.candidate import Candidate
+
+
+class MutationEdge(MutationAlgorithm):
+    def mutate(self, candidate: Candidate):
+        if not self.should_mutate():
+            return candidate
+
+        for chromosome in candidate.chromosomes:
+            num_of_bit = random.randint(0, len(chromosome.gen_list) - 1)
+            # chromosome.gen_list[num_of_bit] = 0 if chromosome.gen_list[num_of_bit] == 1 else 1
+            chromosome.gen_list[num_of_bit] = 1 - chromosome.gen_list[num_of_bit]
+
+        return candidate
--- logic/mutation/mutation_factory.py
+++ logic/mutation/mutation_factory.py
@@ -1,6 +1,7 @@
 from strenum import StrEnum
 from enum import auto
 from logic.mutation.mutation_algorithm import MutationAlgorithm
+from logic.mutation.mutation_edge import MutationEdge
 from logic.mutation.mutation_one_point import MutationOnePoint
 from logic.mutation.mutation_two_points import MutationTwoPoints
 
@@ -8,6 +9,7 @@ from logic.mutation.mutation_two_points import MutationTwoPoints
 class MutationMethod(StrEnum):
     ONE_POINT = auto()
     TWO_POINTS = auto()
+    EDGE = auto()
 
 
 class MutationFactory:
@@ -18,5 +20,7 @@ class MutationFactory:
                 return MutationOnePoint(options.mutation_probability)
             case MutationMethod.TWO_POINTS:
                 return MutationTwoPoints(options.mutation_probability)
+            case MutationMethod.EDGE:
+                return MutationEdge(options.mutation_probability)
             case _:
                 raise Exception("")
